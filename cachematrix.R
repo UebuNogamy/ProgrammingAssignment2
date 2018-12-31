190cac7a70a51070957417f7f71091381fc0585c
UebuNogamy <44441979+UebuNogamy@users.noreply.github.com>
2018-12-31 17:45:46 +0500

    File "cachematrix.R" was changed

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	## Sets initial matrix in special "matrix" object
+	set <- function(y)
+	{
+		x <<- y
+		inv <<- NULL
+	}
+	## Returns initial matrix from special "matrix" object
+	get <- function() x
+	## Caches inverted matrix in special "matrix" object
+	setInvMatrix <- function(inverted) inv <<- inverted
+	## Returns inverted matrix from special "matrix" object
+	getInvMatrix <- function() inv
+	## Creating a special "matrix" object
+	list(get = get, set = set, getInvMatrix = getInvMatrix, setInvMatrix = setInvMatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+	## Gets cached value of inverted matrix from special "matrix" object
+	inv <- x$getInvMatrix
+	## Checks is inverted matrix had already calculated
+	if(!is.null(inv))
+	{
+		## If inversed matrix is already calculated get it from cache
+		message("Getting matrix!")
+		return(inv)
+	}
+	## If inversed matrix is not calculated then get initial matrix and calculate it
+	buffer <- x$get()
+	inv <- solve(buffer, ...)
+	x$setInvMatrix(inv)
+	inv
 }
